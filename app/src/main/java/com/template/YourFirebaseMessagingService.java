5da72da9127a2a76c3f181c3a0ce95f0b93b1c4c
Rushan Kashapov <kashapovrush@mail.ru>
2023-07-08 07:19:52 +0300

    08.07.2023

---
+++ app/src/main/java/com/template/YourFirebaseMessagingService.java
@@ -0,0 +1,8 @@
+package com.template;
+
+import android.app.Service;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class YourFirebaseMessagingService extends FirebaseMessagingService {
+}
